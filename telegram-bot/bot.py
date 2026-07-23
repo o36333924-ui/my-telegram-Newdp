19a2b5ea488f7ff746ecf1ce619c9bf995f3ad3e
o36333924-ui <o36333924@gmail.com>
2026-07-23 09:47:38 +0300

    Update bot.py

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 TOKEN = "8356262671:AAFpw2GxPp7_DAnFDPX45cn6lr3f3AXUffY"
-FILE_ID = "BQACAgEAAxkBAAJa_2phsCu6J7EHpQFUQoDCzknJgZNHAAKyCAACxjEQRwT3gJsV7yDQPQQ"
+FILE_ID = "BQACAgEAAxkBAAJbKGphuP6Uffum9GBB9gWhZBPRz59vAAIEBwAC0kAQR7weTPrVZc1SPQQ"
 
 # ===== КАНАЛЫ ДЛЯ ПОДПИСКИ =====
 CHANNELS = [
