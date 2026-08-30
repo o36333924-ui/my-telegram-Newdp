4e4d2a8ab7d5cb51e3544318ab9b4ee1fe8abac9
o36333924-ui <o36333924@gmail.com>
2026-08-30 15:39:57 +0300

    Update bot.py

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -7,7 +7,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TOKEN = "8356262671:AAFpw2GxPp7_DAnFDPX45cn6lr3f3AXUffY"
+TOKEN = "8915090375:AAESoUc7a3uzaSYNG0PtkV-pwx_W5kDB5OY"
 FILE_ID = "BQACAgEAAxkBAAJdRmp0ujTeHaC5k4Gsv_fXLwpCvzYaAAK7BQACuLqoR5b6TT2cmyrOPQQ"
 
 # ===== КАНАЛЫ ДЛЯ ПОДПИСКИ =====
